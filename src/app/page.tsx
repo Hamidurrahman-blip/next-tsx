04a87dbc486c873220de025c9c7f4db415d2b6b8
Hamidurrahman-blip <vamp521622@gmail.com>
2024-08-29 11:49:49 +0530

    new branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home({status}:{status: string}){
             
         <Header/>
             <h1 className={`${matemasie.className} text-7xl`}>welcome home!</h1>
-<Link href="/about">About</Link>
+<Link href="/about">cAbout</Link>
 <div
   className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
 />
